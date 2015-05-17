69dc0b7d091e47caa68b9e236562e9c773462d76
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-05-17 11:25:33 +0300

    added red test case shouldGetNameReturnJohny at UserTest

--- src/test/java/ua/alekstar/FirstChatUsingTDD/UserTest.java
+++ src/test/java/ua/alekstar/FirstChatUsingTDD/UserTest.java
@@ -10,4 +10,10 @@ public class UserTest {
         User aleks = User.create("Aleks");
         assertEquals("Aleks", aleks.getName());
     }
+
+    @Test
+    public void shouldGetNameReturnJohny() {
+        User johny = User.create("Johny");
+        assertEquals("Johny", johny.getName());
+    }
 }
